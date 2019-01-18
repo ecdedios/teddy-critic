7bbd57915c4422aabbe0247879f6baa8c969d3a3
Ednalyn C. De Dios <ednalyn.dedios@gmail.com>
2019-01-18 09:16:06 -0600

    updates description
    and also the comments

--- teddy.php
+++ teddy.php
@@ -1,20 +1,20 @@
 <?php
 /**
  * @package Teddy_Critic
- * @version 1.17
+ * @version 1.2
  */
 /*
 Plugin Name: Teddy Critic
 Plugin URI: https://github.com/ecdedios/teddy-critic
-Description: A plugin inspired by <cite>Matt's Mullenweg's</cite> original <cite>Hello Dolly</cite> plugin.
+Description: A plugin inspired by <cite>Matt's Mullenweg's</cite> original <cite>Hello Dolly</cite> plugin. When activated a randomly selected phrase from <cite>Teddy Roosevelt's The Man in the Arena"</cite> will appear in the upper right of your admin screen on every page.
 Author: Ednalyn C. De Dios
-Version: 1.17
+Version: 1.2
 Author URI: http://ednalyn.com
 */
 
-function Teddy_Critic_get_quote() {
-    /** This  quote from our dear ol' friend Theodore Roosevelt */
-  $quotes = "It is not the critic who counts;
+function teddy_critic_get_quote() {
+	/** This  is the sppech, The Man in the Arena **/
+	$quotes = "It is not the critic who counts;
 not the man who points out how the strong man stumbles,
 or where the doer of deeds could have done them better.
 The credit belongs to the man who is actually in the arena,
@@ -34,31 +34,30 @@ so that his place shall never be
 with those cold and timid souls
 who neither know victory nor defeat.";
 
-	// Here we split it into lines
+	// Splits it the speech into lines
 	$quotes = explode( "\n", $quotes );
 
-	// And then randomly choose a line
+	// Random selection of a line
 	return wptexturize( $quotes[ mt_rand( 0, count( $quotes ) - 1 ) ] );
 }
 
-// This just echoes the chosen line, we'll position it later
-function Teddy_Critic() {
-	$chosen = Teddy_Critic_get_quote();
-	echo "<p id='dolly'>$chosen</p>";
+// Echoes the chosen line
+function teddy_critic() {
+	$chosen = teddy_critic_get_quote();
+	echo "<p id='teddy'>$chosen</p>";
 }
 
-// Now we set that function up to execute when the admin_notices action is called
-add_action( 'admin_notices', 'Teddy_Critic' );
+// This function will execute when the admin_notices action is called
+add_action( 'admin_notices', 'teddy_critic' );
 
-// We need some CSS to position the paragraph
-// using the original Hello Dolly plugin by Matt
-function dolly_css() {
-	// This makes sure that the positioning is also good for right-to-left languages
+// CSS to position the paragraph
+function teddy_css() {
+	// Ensures positioning is also good for right-to-left languages
 	$x = is_rtl() ? 'left' : 'right';
 
 	echo "
 	<style type='text/css'>
-	#dolly {
+	#teddy {
 		float: $x;
 		padding-$x: 15px;
 		padding-top: 5px;		
@@ -69,6 +68,6 @@ function dolly_css() {
 	";
 }
 
-add_action( 'admin_head', 'dolly_css' );
+add_action( 'admin_head', 'teddy_css' );
 
 ?>
